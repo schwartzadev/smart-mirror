e7d1b2b7c86f0071288c761514560e984f8f499c
Andrew Schwartz <andrew@aschwartz.me>
2017-10-27 09:20:16 -0600

    rm unused imports

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,25 +1,12 @@
 /**
  * Created by Andrew Schwartz on 10/26/17.
  */
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.util.Duration;
-import javafx.animation.Animation;
-
-import java.util.Date;
-import java.util.stream.Stream;
-
-import static javafx.stage.StageStyle.UNDECORATED;
-
 
 public class Window extends Application {
 
