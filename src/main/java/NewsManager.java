3837ee54951cf9406f5a29c414045ab114c620f1
Andrew Schwartz <andrew@aschwartz.me>
2017-10-29 17:14:42 -0600

    add update cycle to news manager

--- src/main/java/NewsManager.java
+++ src/main/java/NewsManager.java
@@ -29,7 +29,7 @@ public class NewsManager {
     private final String apiKey;
     private final String source;
     private final String sortBy; // either 'top' or 'latest'
-    private List<Text> headlines = new ArrayList<Text>();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
 
     public NewsManager(ConfigNews cfn) {
         this.source = cfn.getSource();
@@ -40,57 +40,20 @@ public class NewsManager {
     void add(final Scene scene) {
         Timeline tl = new Timeline();
 
-        VBox container = new VBox();
-
-        this.news = this.update();
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-//        System.out.println(sdf.format(new Date()));
-
-        for (int i = 0; i < 5; i++) {
-            Text time = new Text();
-
-            Date result = parseDate(sdf, this.news.articles.get(i).publishedAt);
-
-            long diff = result.getTime() - new Date().getTime();
-            long minutes = TimeUnit.MILLISECONDS.toMinutes(diff);
-            long hours = TimeUnit.MILLISECONDS.toHours(diff);
-            long days = TimeUnit.MILLISECONDS.toDays(diff);
-
-            if (Math.abs(days) >= 1) {
-                time.setText(Math.abs(days) + "d");
-            } else if (Math.abs(minutes) >= 60) { // TODO add day option
-                time.setText(Math.abs(hours) + "h");
-            } else {
-                time.setText(Math.abs(minutes) + "m");
-            }
-
-            time.getStyleClass().add("time");
-
-            Image newsIcon = new Image("news.png");
-
-            HBox hBox = new HBox(new ImageView(newsIcon), new Text(this.news.articles.get(i).title), time); // TODO shorten article name to n characters
-            hBox.setAlignment(Pos.BASELINE_LEFT);
-
-            hBox.setSpacing(20);
-            container.getChildren().add(hBox);
-//            headlines.add(new Text(this.news.articles.get(i).title));
-        }
-        container.setSpacing(10);
-//        container.getChildren().addAll(headlines);
-
-        container.setPadding(new Insets(20, 20, 20, 80));
-        container.setId("news-container");
-
+        VBox container = cycle();
         final VBox root = (VBox)scene.getRoot();
         root.getChildren().add(container);
 
-        tl.setCycleCount(Animation.INDEFINITE);
-        KeyFrame updateTime = new KeyFrame(Duration.seconds(2), event -> {
-            // TODO: add update cycle
-        });
 
+        this.sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        tl.setCycleCount(Animation.INDEFINITE);
+        KeyFrame updateTime = new KeyFrame(Duration.seconds(60 * 2), event -> {
+            VBox v = cycle();
+            root.getChildren().remove(root.lookup("#news-container"));
+            root.getChildren().add(v);
+            System.out.println("updated news");
+        });
         tl.getKeyFrames().add(updateTime);
         tl.play();
     }
@@ -105,6 +68,43 @@ public class NewsManager {
         return published;
     }
 
+    private VBox cycle() {
+        VBox container = new VBox();
+        this.news = this.update();
+        for (int i = 0; i < 5; i++) {
+            Text time = new Text();
+
+            Date result = parseDate(this.sdf, this.news.articles.get(i).publishedAt);
+
+            long diff = result.getTime() - new Date().getTime();
+            long minutes = TimeUnit.MILLISECONDS.toMinutes(diff);
+            long hours = TimeUnit.MILLISECONDS.toHours(diff);
+            long days = TimeUnit.MILLISECONDS.toDays(diff);
+
+            if (Math.abs(days) >= 1) {
+                time.setText(Math.abs(days) + "d");
+            } else if (Math.abs(minutes) >= 60) {
+                time.setText(Math.abs(hours) + "h");
+            } else {
+                time.setText(Math.abs(minutes) + "m");
+            }
+
+            time.getStyleClass().add("time");
+
+            Image newsIcon = new Image("news.png");
+
+            HBox hBox = new HBox(new ImageView(newsIcon), new Text(this.news.articles.get(i).title), time); // TODO shorten article name to n characters
+            hBox.setAlignment(Pos.BASELINE_LEFT);
+
+            hBox.setSpacing(20);
+            container.getChildren().add(hBox);
+        }
+        container.setSpacing(10);
+        container.setPadding(new Insets(20, 20, 20, 80));
+        container.setId("news-container");
+        return container;
+    }
+
     private News update() {
         String raw = null;
         try {
@@ -115,6 +115,7 @@ public class NewsManager {
             e.printStackTrace();
             this.news = null;
         }
+        System.out.println(raw);
         return getNews();
     }
 
