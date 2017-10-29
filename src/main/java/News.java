b0d445fbd913eae5e4514bd5ab78fb3becd01f7c
Andrew Schwartz <andrew@aschwartz.me>
2017-10-29 00:24:11 -0600

    add News, Article classes

--- src/main/java/News.java
+++ src/main/java/News.java
@@ -1,7 +1,33 @@
-package PACKAGE_NAME;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
 
 /**
  * Created by Andrew Schwartz on 10/29/17.
  */
 public class News {
+    @SerializedName("status")
+    public String status;
+    @SerializedName("source")
+    public String source;
+    @SerializedName("sortBy")
+    public String sortBy;
+    @SerializedName("articles")
+    public List<Article> articles = null;
+}
+
+class Article {
+    @SerializedName("author")
+    public String author;
+    @SerializedName("title")
+    public String title;
+    @SerializedName("description")
+    public String description;
+    @SerializedName("url")
+    public String url;
+    @SerializedName("urlToImage")
+    public String urlToImage;
+    @SerializedName("publishedAt")
+    public String publishedAt;
 }
